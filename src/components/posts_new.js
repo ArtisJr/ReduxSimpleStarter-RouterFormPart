fdcf675eaa7e51a5b93f4fc4ab3a068f1cb64e1d
Artis Coleman <artis.coleman@universalorlando.com>
2018-03-14 18:03:31 -0400

    Finished 138

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
-        <input type="text" {...field.input} />
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
     );
   }
@@ -13,12 +15,41 @@ class PostsNew extends Component {
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderTitleField} />
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    error.title = 'Enter a title!';
+  }
+
+  if (!values.categories) {
+    error.categories = 'Enter some categories!';
+  }
+
+  if (!values.content) {
+    error.content = 'Enter some content!';
+  }
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
